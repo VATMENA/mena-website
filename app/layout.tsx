0a3f24fa2b52072c9c3d8569cb4bf1c76c99c12d
Samuel Hepworth <samhep0803@gmail.com>
2024-01-16 22:30:34 +0400

    comment out stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         <div>
           <Footer />
         </div>
-        <CountdownTimer />
+        // <CountdownTimer />
         <Analytics />
       </body>
     </html>
--- components/progress-bar/timer.tsx
+++ components/progress-bar/timer.tsx
@@ -1,19 +1,24 @@
 "use client";
 
-import { differenceInDays, differenceInMilliseconds } from "date-fns";
+import { differenceInDays } from "date-fns";
 import { useEffect, useState } from "react";
 import { Progress } from "../ui/progress";
 import { Card, CardContent, CardHeader } from "../ui/card";
 
-export const CountdownTimer: React.FC<{}> = ({ }) => {
+export const CountdownTimer: React.FC<{}> = ({}) => {
   const [percentage, setPercentage] = useState(0);
+  const [days, setDays] = useState(0);
 
   useEffect(() => {
     const timer = setInterval(() => {
-      const diff = differenceInDays(new Date(), new Date(2024, 2, 2, 19));
+      const diff = differenceInDays(
+        new Date(2024, 1, 4),
+        new Date(2024, 2, 2, 19),
+      );
+      setDays(diff * -1);
 
       const startDiff = differenceInDays(
-        new Date(2024, 0, 15),
+        new Date(2024, 1, 3, 19),
         new Date(2024, 2, 2, 19),
       );
 
@@ -27,9 +32,10 @@ export const CountdownTimer: React.FC<{}> = ({ }) => {
     <Card className="fixed bottom-2 right-2 bg-card/50 backdrop-blur-lg">
       <CardHeader className="font-bold">Watch this space...</CardHeader>
       <CardContent>
-        <div className="flex items-center text-center gap-x-2 w-72">
+        <div className="grid grid-cols-6 items-center text-center gap-x-2 w-72">
           {percentage}%
-          <Progress value={percentage} className="w-full" />
+          <Progress value={percentage} className="w-full col-span-5" />
+          <div className="col-start-2 col-span-5">{days} days remain.</div>
         </div>
       </CardContent>
     </Card>
