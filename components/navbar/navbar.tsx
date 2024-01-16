cb0cd9ce6cd8c2c0572de8a4ca624b4c95c7fa70
Samuel Hepworth <samhep0803@gmail.com>
2024-01-16 18:45:30 +0400

    use newer vatmena logo

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,52 +1,60 @@
 "use client";
 
-import React, { useEffect, useState } from 'react'
-import Image from 'next/image'
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
 import { cn } from "@/lib/utils";
 import MobileNavbar from "./mobile-navbar";
 import NavbarItems from "./navbar-items";
 import Link from "next/link";
 
-
 function Navbar() {
+  const [hasScrolled, setHasScrolled] = useState(false);
 
-    const [hasScrolled, setHasScrolled] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
 
-    useEffect(() => {
-        window.addEventListener("scroll", handleScroll, { passive: true })
+    // cleanup
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-        // cleanup
-        return () => {
-            window.removeEventListener("scroll", handleScroll)
-        }
-    }, [])
-
-    const handleScroll = () => {
-        const pos = window.scrollY;
-        if (pos > 1) {
-            setHasScrolled(true);
-        } else {
-            setHasScrolled(false);
-        }
+  const handleScroll = () => {
+    const pos = window.scrollY;
+    if (pos > 1) {
+      setHasScrolled(true);
+    } else {
+      setHasScrolled(false);
     }
+  };
 
-    return (
-        <div className={cn("transition ease-in-out duration-300 fixed w-full py-6", hasScrolled ? "bg-background shadow-2xl" : "bg-transparent")}>
-            <div className="container mx-auto">
-                <div className="float-left">
-                    <Link href="/"><Image src="/logo.png" height={38} width={141} alt="Logo" /></Link>
-                </div>
-                <div className="float-right">
-                    <div className="md:hidden">
-                        <MobileNavbar />
-                    </div>
-                    <div className="hidden md:block">
-                        <NavbarItems />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div
+      className={cn(
+        "transition ease-in-out duration-300 fixed w-full py-6",
+        hasScrolled ? "bg-background shadow-2xl" : "bg-transparent",
+      )}
+    >
+      <div className="flex container justify-between items-center">
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-full h-16"
+          />
+        </Link>
+        <div className="md:hidden">
+          <MobileNavbar />
         </div>
-    )
+        <div className="hidden md:block">
+          <NavbarItems />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
