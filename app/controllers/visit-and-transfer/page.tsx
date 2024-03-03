28758d46e5779e97236263a1a8dd083887b0cf74
Samuel Hepworth <samhep0803@gmail.com>
2024-03-03 15:24:30 +0400

    update all cdn links and copyright year

--- app/controllers/visit-and-transfer/page.tsx
+++ app/controllers/visit-and-transfer/page.tsx
@@ -1,107 +1,185 @@
-import Header from "@/components/header/header"
-import { buttonVariants } from "@/components/ui/button"
-import Link from "next/link"
-import React from 'react'
-import { BiLinkExternal } from "react-icons/bi"
+import Header from "@/components/header/header";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+import { BiLinkExternal } from "react-icons/bi";
 
 function VisitTransfer() {
-    return (
-        <>
-            <Header imageUrl="/images/atc-scope-2.png">
-                <h1 className="text-5xl">Visit & Transfer</h1>
-            </Header>
-            <div className="container mx-auto">
-                <div className="mb-12">
-                    <h2 className="text-3xl font-bold">Visiting</h2>
-                    <p>Looking to stay in your current Division/vACC as a resident but interested in controlling in the Middle East and North Africa?</p>
-                    <p>VATMENA host a range of vACCs offering visiting spaces for controllers interested in an adventure.</p>
-                    <p>Every visitor is subject to both VATSIM&apos;s and VATMENA&apos;s Visiting Controller policy.</p>
-                    <div className="flex flex-wrap gap-3 mt-6">
-                        <Link href="https://vatsim.net/docs/policy/transfer-and-visiting-controller-policy#2-visiting-controller"
-                            target="_blank"
-                            className={buttonVariants({ variant: "default" })}
-                        >
-                            <div>VATSIM Visiting and Transfer Policy <BiLinkExternal className="inline" /></div>
-                        </Link>
-                        <Link href="https://cdn.vatsim.me/policies/VATMENA_POL_MP_R01-2022.pdf"
-                            target="_blank"
-                            className={buttonVariants({ variant: "default" })}
-                        >
-                            <div>VATMENA Membership Policy <BiLinkExternal className="inline" /></div>
-                        </Link>
-                    </div>
-                    <p className="text-foreground/50 mt-1"><em>See section 9 of the VATMENA Membership Policy for details about visiting.</em></p>
-                </div>
-                <div>
-                    <h2 className="text-3xl font-bold">Transferring</h2>
-                    <p>Fancy a change and want to become a resident of VATMENA?</p>
-                    <p>See a list of our <Link href="/division/vaccs" className="italic font-bold hover:underline text-primary">vACCs</Link> to find your new home.</p>
-                    <div className="mt-6">
-                        <p>Follow this list of steps to start your new journey.</p>
+  return (
+    <>
+      <Header imageUrl="/images/atc-scope-2.png">
+        <h1 className="text-5xl">Visit & Transfer</h1>
+      </Header>
+      <div className="container mx-auto">
+        <div className="mb-12">
+          <h2 className="text-3xl font-bold">Visiting</h2>
+          <p>
+            Looking to stay in your current Division/vACC as a resident but
+            interested in controlling in the Middle East and North Africa?
+          </p>
+          <p>
+            VATMENA host a range of vACCs offering visiting spaces for
+            controllers interested in an adventure.
+          </p>
+          <p>
+            Every visitor is subject to both VATSIM&apos;s and VATMENA&apos;s
+            Visiting Controller policy.
+          </p>
+          <div className="mt-6 flex flex-wrap gap-3">
+            <Link
+              href="https://vatsim.net/docs/policy/transfer-and-visiting-controller-policy#2-visiting-controller"
+              target="_blank"
+              className={buttonVariants({ variant: "default" })}
+            >
+              <div>
+                VATSIM Visiting and Transfer Policy{" "}
+                <BiLinkExternal className="inline" />
+              </div>
+            </Link>
+            <Link
+              href="https://cdn.vatsim.me/policies/VATMENA_POL_MP_R01-2024.pdf"
+              target="_blank"
+              className={buttonVariants({ variant: "default" })}
+            >
+              <div>
+                VATMENA Membership Policy <BiLinkExternal className="inline" />
+              </div>
+            </Link>
+          </div>
+          <p className="text-foreground/50 mt-1">
+            <em>
+              See section 9 of the VATMENA Membership Policy for details about
+              visiting.
+            </em>
+          </p>
+        </div>
+        <div>
+          <h2 className="text-3xl font-bold">Transferring</h2>
+          <p>Fancy a change and want to become a resident of VATMENA?</p>
+          <p>
+            See a list of our{" "}
+            <Link
+              href="/division/vaccs"
+              className="text-primary font-bold italic hover:underline"
+            >
+              vACCs
+            </Link>{" "}
+            to find your new home.
+          </p>
+          <div className="mt-6">
+            <p>Follow this list of steps to start your new journey.</p>
 
-                        {/* start review docs */}
-                        <div className="mt-3">
-                            <h3 className="text-xl font-bold">1. Review Transfer Policies</h3>
-                            <p>All members wishing to transfer to one of our vACCs are subject to the following policies.</p>
-                            <ul>
-                                <li>
-                                    <Link className="italic font-bold hover:underline text-primary" target="_blank" href="https://vatsim.net/docs/policy/global-ratings-policy">
-                                        VATSIM Global Ratings Policy <BiLinkExternal className="inline" />
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link className="italic font-bold hover:underline text-primary" target="_blank" href="https://vatsim.net/docs/policy/transfer-and-visiting-controller-policy#1-region-transfer">
-                                        VATSIM Transfer & Visiting Policy <BiLinkExternal className="inline" />
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link className="italic font-bold hover:underline text-primary" target="_blank" href="https://cdn.vatsim.me/policies/VATMENA_POL_MP_R01-2022.pdf">
-                                        VATMENA Membership Policy <BiLinkExternal className="inline" />
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                        {/* end review docs */}
-
-                        {/* start transfer */}
-                        <div className="mt-3">
-                            <h3 className="text-xl font-bold">2. Initiate Division Transfer</h3>
-                            <p>If you are a part of the Europe, Middle East, and Africa region, then join our&nbsp;
-                                <Link href="https://discord.com/invite/Hvxm5Ky" target="_blank" className="italic font-bold hover:underline text-primary">Discord <BiLinkExternal className="inline" /></Link>&nbsp;
-                                and submit a support ticket.
-                            </p>
-                            <p>If you are not a member of the Europe, Middle East, and Africa region, a transfer request should be submitted on&nbsp;
-                                <Link href="https://my.vatsim.net/user/region" target="_blank" className="italic font-bold hover:underline text-primary">My VATSIM <BiLinkExternal className="inline" /></Link>
-                            </p>
-                        </div>
-                        {/* end transfer */}
-
-                        {/* review vacc assignment */}
-                        <div className="mt-3">
-                            <h3 className="text-xl font-bold">3. vACC Assignment</h3>
-                            <p>
-                                Once your transfer to VATMENA has been finalized, you will need to request assignment
-                                to one of our vACCs via <Link href="https://discord.com/invite/Hvxm5Ky" target="_blank" className="italic font-bold hover:underline text-primary">Discord <BiLinkExternal className="inline" /></Link>.&nbsp;
-                            </p>
-                            <p>
-                                Upon approval of the request, you will receive a welcome introduction in response to your
-                                ticket that contains your chosen vACC&apos;s contact information.
-                            </p>
-                        </div>
-                        {/* end vacc assignment */}
-
-                        {/* start local training */}
-                        <div className="mt-3">
-                            <h3 className="text-xl font-bold">4. Local Training</h3>
-                            <p>You can then contact your new vACC&apos;s Training Department to get started with your controller training.</p>
-                            <p>Good luck, we hope to see you soon!</p>
-                        </div>
-                        {/* end local airspace */}
-                    </div>
-                </div>
+            {/* start review docs */}
+            <div className="mt-3">
+              <h3 className="text-xl font-bold">1. Review Transfer Policies</h3>
+              <p>
+                All members wishing to transfer to one of our vACCs are subject
+                to the following policies.
+              </p>
+              <ul>
+                <li>
+                  <Link
+                    className="text-primary font-bold italic hover:underline"
+                    target="_blank"
+                    href="https://vatsim.net/docs/policy/global-ratings-policy"
+                  >
+                    VATSIM Global Ratings Policy{" "}
+                    <BiLinkExternal className="inline" />
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="text-primary font-bold italic hover:underline"
+                    target="_blank"
+                    href="https://vatsim.net/docs/policy/transfer-and-visiting-controller-policy#1-region-transfer"
+                  >
+                    VATSIM Transfer & Visiting Policy{" "}
+                    <BiLinkExternal className="inline" />
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="text-primary font-bold italic hover:underline"
+                    target="_blank"
+                    href="https://cdn.vatsim.me/policies/VATMENA_POL_MP_R01-2024.pdf"
+                  >
+                    VATMENA Membership Policy{" "}
+                    <BiLinkExternal className="inline" />
+                  </Link>
+                </li>
+              </ul>
             </div>
-        </>
-    )
+            {/* end review docs */}
+
+            {/* start transfer */}
+            <div className="mt-3">
+              <h3 className="text-xl font-bold">
+                2. Initiate Division Transfer
+              </h3>
+              <p>
+                If you are a part of the Europe, Middle East, and Africa region,
+                then join our&nbsp;
+                <Link
+                  href="https://discord.com/invite/Hvxm5Ky"
+                  target="_blank"
+                  className="text-primary font-bold italic hover:underline"
+                >
+                  Discord <BiLinkExternal className="inline" />
+                </Link>
+                &nbsp; and submit a support ticket.
+              </p>
+              <p>
+                If you are not a member of the Europe, Middle East, and Africa
+                region, a transfer request should be submitted on&nbsp;
+                <Link
+                  href="https://my.vatsim.net/user/region"
+                  target="_blank"
+                  className="text-primary font-bold italic hover:underline"
+                >
+                  My VATSIM <BiLinkExternal className="inline" />
+                </Link>
+              </p>
+            </div>
+            {/* end transfer */}
+
+            {/* review vacc assignment */}
+            <div className="mt-3">
+              <h3 className="text-xl font-bold">3. vACC Assignment</h3>
+              <p>
+                Once your transfer to VATMENA has been finalized, you will need
+                to request assignment to one of our vACCs via{" "}
+                <Link
+                  href="https://discord.com/invite/Hvxm5Ky"
+                  target="_blank"
+                  className="text-primary font-bold italic hover:underline"
+                >
+                  Discord <BiLinkExternal className="inline" />
+                </Link>
+                .&nbsp;
+              </p>
+              <p>
+                Upon approval of the request, you will receive a welcome
+                introduction in response to your ticket that contains your
+                chosen vACC&apos;s contact information.
+              </p>
+            </div>
+            {/* end vacc assignment */}
+
+            {/* start local training */}
+            <div className="mt-3">
+              <h3 className="text-xl font-bold">4. Local Training</h3>
+              <p>
+                You can then contact your new vACC&apos;s Training Department to
+                get started with your controller training.
+              </p>
+              <p>Good luck, we hope to see you soon!</p>
+            </div>
+            {/* end local airspace */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default VisitTransfer
+export default VisitTransfer;
--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,52 +1,137 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from "next/link"
-import { Separator } from "../ui/separator"
-import { BiLinkExternal } from "react-icons/bi"
-import { links } from "@/lib/config"
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Separator } from "../ui/separator";
+import { BiLinkExternal } from "react-icons/bi";
+import { links } from "@/lib/config";
 
 function Footer() {
-    return (
-        <>
-            <footer className="lg:hidden flex flex-col justify-center p-3">
-                <p className="text-center">&copy; VATMENA 2023 | Content on this website is for flight simulation purposes only.</p>
-            </footer>
-            <footer className="container mx-auto p-10 hidden lg:block">
-                <Separator className="my-6" />
-                <div className="grid grid-cols-4">
-                    <div className="col-span-1">
-                        <Image src="/logo.png" height={38} width={141} alt="Logo" className="pb-3" />
-                        <p className="pb-3">&copy; VATMENA 2023<br />Content on this website is for<br />flight simulation purposes only.</p>
-                        <Link href="https://cdn.vatsim.me/policies/VATMENA_POL_DPP_R01-2023.pdf" target="_blank" className="hover:underline">Privacy Policy</Link>
-                    </div>
-                    <nav className="col-span-1 flex flex-col">
-                        <header className="pb-6 uppercase">Division</header>
-                        <Link href="/division/join" className="hover:underline">Join Us</Link>
-                        <Link href="/division/vaccs" className="hover:underline">Area Control Centers</Link>
-                        <Link href="/division/policies-downloads" className="hover:underline">Policies & Downloads</Link>
-                        <Link href="/division/staff" className="hover:underline">Staff</Link>
-                        <Link href={links.academy} target="_blank" className="hover:underline">Academy <BiLinkExternal className="inline" /></Link>
-                        <Link href={links.discord} target="_blank" className="hover:underline">Discord <BiLinkExternal className="inline" /></Link>
-                    </nav>
-                    <nav className="col-span-1 flex flex-col">
-                        <header className="pb-6 uppercase">Pilots</header>
-                        <Link href="/pilots/resources" className="hover:underline">Resources</Link>
-                        <Link href="https://my.vatsim.net/pilots/train" target="_blank" className="hover:underline">Pilot Training <BiLinkExternal className="inline" /></Link>
-                        <Link href="https://my.vatsim.net/virtual-airlines" target="_blank" className="hover:underline">Virtual Airlines <BiLinkExternal className="inline" /></Link>
-                        <Link href="https://my.vatsim.net" target="_blank" className="hover:underline">myVATSIM <BiLinkExternal className="inline" /></Link>
-                    </nav>
-                    <nav className="col-span-1 flex flex-col">
-                        <header className="pb-6 uppercase">Controllers</header>
-                        <Link href="/controllers/become-a-controller" className="hover:underline">Become a Controller</Link>
-                        <Link href="/controllers/visit-and-transfer" className="hover:underline">Visit & Transfer</Link>
-                        <Link href="https://staff.vatsim.me/instructors" target="_blank" className="hover:underline">Training Staff <BiLinkExternal className="inline" /></Link>
-                        <Link href="https://staff.vatsim.me/solos" target="_blank" className="hover:underline">Active Validations <BiLinkExternal className="inline" /></Link>
-                    </nav>
-                </div>
-            </footer>
-        </>
-
-    )
+  return (
+    <>
+      <footer className="flex flex-col justify-center p-3 lg:hidden">
+        <p className="text-center">
+          &copy; VATMENA 2023 | Content on this website is for flight simulation
+          purposes only.
+        </p>
+      </footer>
+      <footer className="container mx-auto hidden p-10 lg:block">
+        <Separator className="my-6" />
+        <div className="grid grid-cols-4">
+          <div className="col-span-1">
+            <Image
+              src="/logo.png"
+              height={38}
+              width={141}
+              alt="Logo"
+              className="pb-3"
+            />
+            <p className="pb-3">
+              &copy; VATMENA 2024
+              <br />
+              Content on this website is for
+              <br />
+              flight simulation purposes only.
+            </p>
+            <Link
+              href="https://cdn.vatsim.me/policies/VATMENA_POL__DPP_R01-2024.pdf"
+              target="_blank"
+              className="hover:underline"
+            >
+              Privacy Policy
+            </Link>
+          </div>
+          <nav className="col-span-1 flex flex-col">
+            <header className="pb-6 uppercase">Division</header>
+            <Link href="/division/join" className="hover:underline">
+              Join Us
+            </Link>
+            <Link href="/division/vaccs" className="hover:underline">
+              Area Control Centers
+            </Link>
+            <Link
+              href="/division/policies-downloads"
+              className="hover:underline"
+            >
+              Policies & Downloads
+            </Link>
+            <Link href="/division/staff" className="hover:underline">
+              Staff
+            </Link>
+            <Link
+              href={links.academy}
+              target="_blank"
+              className="hover:underline"
+            >
+              Academy <BiLinkExternal className="inline" />
+            </Link>
+            <Link
+              href={links.discord}
+              target="_blank"
+              className="hover:underline"
+            >
+              Discord <BiLinkExternal className="inline" />
+            </Link>
+          </nav>
+          <nav className="col-span-1 flex flex-col">
+            <header className="pb-6 uppercase">Pilots</header>
+            <Link href="/pilots/resources" className="hover:underline">
+              Resources
+            </Link>
+            <Link
+              href="https://my.vatsim.net/pilots/train"
+              target="_blank"
+              className="hover:underline"
+            >
+              Pilot Training <BiLinkExternal className="inline" />
+            </Link>
+            <Link
+              href="https://my.vatsim.net/virtual-airlines"
+              target="_blank"
+              className="hover:underline"
+            >
+              Virtual Airlines <BiLinkExternal className="inline" />
+            </Link>
+            <Link
+              href="https://my.vatsim.net"
+              target="_blank"
+              className="hover:underline"
+            >
+              myVATSIM <BiLinkExternal className="inline" />
+            </Link>
+          </nav>
+          <nav className="col-span-1 flex flex-col">
+            <header className="pb-6 uppercase">Controllers</header>
+            <Link
+              href="/controllers/become-a-controller"
+              className="hover:underline"
+            >
+              Become a Controller
+            </Link>
+            <Link
+              href="/controllers/visit-and-transfer"
+              className="hover:underline"
+            >
+              Visit & Transfer
+            </Link>
+            <Link
+              href="https://staff.vatsim.me/instructors"
+              target="_blank"
+              className="hover:underline"
+            >
+              Training Staff <BiLinkExternal className="inline" />
+            </Link>
+            <Link
+              href="https://staff.vatsim.me/solos"
+              target="_blank"
+              className="hover:underline"
+            >
+              Active Validations <BiLinkExternal className="inline" />
+            </Link>
+          </nav>
+        </div>
+      </footer>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
